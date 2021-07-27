8344ac79044f52216cdefbbadbd4f1a467e8fbbf
Austin Summerlin <summerlin.austin@gmail.com>
2021-07-27 15:11:24 -0700

    names only list

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Character = ({ name, image }) => (
+const Character = ({ name }) => (
   <section>
-    <h2>{name}</h2>
-    <img src={image} alt={name} />
+    <p>{name}</p>
   </section>
 );
 
 Character.propTypes = {
   name: PropTypes.string,
-  image: PropTypes.string,
 };
 
 export default Character;
