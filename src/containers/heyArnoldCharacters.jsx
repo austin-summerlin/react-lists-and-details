f9d60636e322afec1929896a212901b0b0eea517
Austin Summerlin <summerlin.austin@gmail.com>
2021-07-27 15:03:47 -0700

    list of characters

--- src/containers/heyArnoldCharacters.jsx
+++ src/containers/heyArnoldCharacters.jsx
@@ -1,32 +1,21 @@
+/* eslint-disable keyword-spacing */
 import React, { Component } from 'react';
-import Character from '../components/Characters';
+import CharacterList from '../components/CharacterList';
 import { fetchCharacters } from '../services/heyArnoldApi';
 
 export default class HeyArnoldCharacters extends Component {
   state = {
-    name: '',
-    image: '',
+    characters: [],
   };
 
-  getCharacters = async () => {
-    const { name, image } = await fetchCharacters();
-    this.setState({ name, image });
-    console.log(this.state);
-  };
-
-  componentDidMount() {
-    this.getCharacters();
+  async componentDidMount() {
+    const characters = await fetchCharacters();
+    this.setState({ characters });
   }
 
   render() {
-    const { name, image } = this.state;
-    return (
-      <>
-        <h1>Characters</h1>
-        <Character name={name} image={image} />
-      </>
-    );
+    const { characters } = this.state;
+
+    return <CharacterList characters={characters} />;
   }
 }
-
-
--- src/services/heyArnoldApi.js
+++ src/services/heyArnoldApi.js
@@ -1,11 +1,8 @@
+/* eslint-disable max-len */
 export const fetchCharacters = async () => {
-  const res = await fetch(`
-  https://hey-arnold-api.herokuapp.com/api/v1/characters/random`);
-  const characters = await res.json();
-  console.log(characters[0].name, characters[0].image);
-  return {
-    name: characters[0].name,
-    image: characters[0].image
-  };
+  const res = await fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters/');
+  const json = await res.json();
+  return json;
+
 };
 
